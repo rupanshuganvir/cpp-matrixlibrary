4da9da485a81f831771812eaea8ac606ab997682
theQmech <rupanshu.ganvir@gmail.com>
2015-04-13 00:32:08 +0530

    Rank and Documentation updated

--- examples/usage.cpp
+++ examples/usage.cpp
@@ -8,10 +8,8 @@ int main(){
 	double A[]= {2,1,1,4,-6,0,-2,7,2};
 	matrix<double> P(3,3,A);
 	cout<<P<<endl;
-
 	cout<<reduced(P)<<endl;
-
 	cout<<C_space(P)<<endl;
-
+	cout<<rank(P)<<endl;
 	return 0;
 }
